06db0585569eb69fae284738a24ceb22e435bcda
Alexander-Baldwin <antonybonn@outlook.com>
2019-11-14 18:34:49 +0800

    first commit

---
+++ usr/themes/echo/footer.php
@@ -0,0 +1,13 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<div class="footer">
+    <div class="layui-col-md12 t-copy">
+        <span class="layui-breadcrumb">
+            <span>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?>.个人博客</a></span>
+        </span>
+    </div>
+</div>
+
+<?php $this->footer(); ?>
+</body>
+</html>
